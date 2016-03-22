adc054a0ffba02ef82744931aeb19fcd05f78e58
mwhatters <mwhatters@gmail.com>
2016-03-22 00:09:53 -0700

    remove whitespace cleanup cam transition script

--- Assets/Scripts/Main/All Worlds/CamShifting.js
+++ Assets/Scripts/Main/All Worlds/CamShifting.js
@@ -1,28 +1,21 @@
 ﻿#pragma strict
 
-
 private var cameraObj : CameraBehavior;
 private var lunaObj : GameObject;
 
-
-private var cameraIsStatic : boolean;
 private var boxesUnfrozen = false;
 public var objectsList : String;
-public var cameraZ : int = -8;
 
+public var cameraZ : int = -8;
 public var y : float;
 public var x : float;
 public var zIn : float;
 public var zOut : float;
-
-public var transitionEnterTime : float;
-public var transitionExitTime : float;
-
+public var transitionEnterTime : float = 1;
+public var transitionExitTime : float = 0.5;
 public var zoomOnly : boolean = false;
 
-
 private var currentlyTransitioning = false;
-
 
 function Start () {
 	cameraObj = GameObject.FindGameObjectWithTag("MainCamera").GetComponent(CameraBehavior);
@@ -31,8 +24,7 @@ function Start () {
 
 function OnTriggerEnter2D(coll : Collider2D) {
 
-	if (coll.name == "Luna") { 
-
+	if (coll.name == "Luna") {
 		Play("ExpandSound");
 
 		if (!boxesUnfrozen && objectsList != "") {
@@ -43,7 +35,6 @@ function OnTriggerEnter2D(coll : Collider2D) {
 			}
 			boxesUnfrozen = true;
 		}
-
 
 		if (currentlyTransitioning) {
 			StopCoroutine("panCameraToFollowLuna");
@@ -61,7 +52,6 @@ function OnTriggerEnter2D(coll : Collider2D) {
 
 function OnTriggerExit2D(coll : Collider2D) {
 	if (coll.name == "Luna") {
-
 		Play("RestrainSound");
 
 		if (currentlyTransitioning) {
@@ -78,8 +68,6 @@ function OnTriggerExit2D(coll : Collider2D) {
 	}
 }
 
-
-
 function panCameraToPoint() {
 
 	var cameraScope = cameraObj.GetComponent(Camera);
@@ -87,7 +75,6 @@ function panCameraToPoint() {
 	var endPos = Vector3(x, y, cameraZ);
 	var rate = 1.0/transitionEnterTime;
 	var t = 0.0;
-
 	currentlyTransitioning = true;
 
 	while (t < 1.0) {
@@ -97,14 +84,12 @@ function panCameraToPoint() {
 		yield;
 	}
 
-	GameObject.FindGameObjectWithTag("ExpandSound").GetComponent(AudioSource).Play();
 	currentlyTransitioning = false;
 }
 
 
 
 function panCameraToFollowLuna() {
-
 	var cameraScope = cameraObj.GetComponent(Camera);
 	var startPos = cameraObj.transform.position;
 	var endPos : Vector3;
@@ -112,18 +97,14 @@ function panCameraToFollowLuna() {
 	var y;
 	var rate = 1.0/transitionExitTime;
 	var t = 0.0;
-
 	currentlyTransitioning = true;
 
 	while (t < 1.0) {
-
 		endPos = Vector3(lunaObj.transform.position.x, lunaObj.transform.position.y, cameraZ);
-
-
 		x = lunaObj.transform.position.x;
 		y = lunaObj.transform.position.y;
-
 		t += Time.deltaTime * rate;
+
 		cameraObj.transform.position = Vector3.Lerp(startPos, endPos, t);
 		cameraScope.orthographicSize = Mathf.Lerp(cameraScope.orthographicSize, zOut, t);
 		yield;
@@ -134,10 +115,8 @@ function panCameraToFollowLuna() {
 
 function zoomCameraEnter() {
 	var cameraScope = cameraObj.GetComponent(Camera);
-
 	var rate = 1.0/transitionEnterTime;
 	var t = 0.0;
-
 	currentlyTransitioning = true;
 
 	while (t < 1.0) {
@@ -152,10 +131,8 @@ function zoomCameraEnter() {
 
 function zoomCameraExit() {
 	var cameraScope = cameraObj.GetComponent(Camera);
-
 	var rate = 1.0/transitionEnterTime;
 	var t = 0.0;
-
 	currentlyTransitioning = true;
 
 	while (t < 1.0) {
