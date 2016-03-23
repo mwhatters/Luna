240bc1be00580ed9d5ee56fa2d41750b6c764e48
mwhatters <mwhatters@gmail.com>
2016-03-22 21:56:50 -0700

    finish up prefab allocation

--- Assets/Scripts/3rd Party Objects/KeyDoor.js
+++ Assets/Scripts/3rd Party Objects/KeyDoor.js
@@ -3,16 +3,22 @@
 public var keyNeeded : String;
 
 function OnCollisionEnter2D(coll : Collision2D) {
-	Debug.Log(coll.gameObject.tag);
 	if (coll.gameObject.tag == "TheGuy") {
+
+		if (keyNeeded == "None") {
+			DestroyDoor();
+			return true;
+		}
 
 		for (var key in coll.gameObject.GetComponent(PlayerGameStates).keysFound) {
 			if (key.name == keyNeeded) {
-				GameObject.FindGameObjectWithTag("DoorOpenSound").GetComponent(AudioSource).Play();
-				Destroy(this.gameObject);
+				DestroyDoor();
 			}
 		}
-
-
 	}
+}
+
+function DestroyDoor() {
+	GameObject.FindGameObjectWithTag("DoorOpenSound").GetComponent(AudioSource).Play();
+	Destroy(this.gameObject);
 }
