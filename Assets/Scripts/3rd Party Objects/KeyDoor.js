20b3749497fae593c1b678f923c965fc5beb36b0
mwhatters <mwhatters@gmail.com>
2016-04-17 19:48:24 -0700

    key tracker

--- Assets/Scripts/3rd Party Objects/KeyDoor.js
+++ Assets/Scripts/3rd Party Objects/KeyDoor.js
@@ -2,7 +2,6 @@
 import System.Linq;
 
 public var keysNeeded : GameObject[];
-private var noKeys : GameObject[];
 
 function OnCollisionEnter2D(coll : Collision2D) {
 	if (coll.gameObject.tag == "TheGuy") {
@@ -11,24 +10,34 @@ function OnCollisionEnter2D(coll : Collision2D) {
 			return true;
 		}
 
-
 		var keys = coll.gameObject.GetComponent(PlayerGameStates).keysFound;
 		var keyCount = 0;
+		var keysToBeDestroyed : GameObject[];
 
 		for (var k in keys) {
 			if (keysNeeded.Contains(k)) {
 				keyCount += 1;
+				keysToBeDestroyed += [k];
 			}
 
 			if (keyCount == keysNeeded.Length) {
 				DestroyDoor();
+				DestroyRelevantKeysFound(keysToBeDestroyed);
 			}
 		}
 	}
+
+	return true;
 }
 
 function DestroyDoor() {
 	Sounds.use.PlaySoundByTag("DoorOpenSound");
 	Destroy(GetComponent(SpriteRenderer));
 	Destroy(GetComponent(BoxCollider2D));
+}
+
+function DestroyRelevantKeysFound(keysToBeDestroyed) {
+	for (var key in keysToBeDestroyed) {
+		Debug.Log(key);
+	}
 }
