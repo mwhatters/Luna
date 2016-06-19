9894ae2928b0b38482872e9c508474f8a7b6f88e
mwhatters <mwhatters@gmail.com>
2016-06-19 14:42:03 -0700

    fizdoor logic improvements

--- Assets/Scripts/3rd Party Objects/FizDoor.js
+++ Assets/Scripts/3rd Party Objects/FizDoor.js
@@ -3,18 +3,22 @@
 public var freezeOnExit : boolean = false;
 private var fizDoor : Transform;
 
+enum exitDirection { Down, Left, Right, Up }
+public var exitFrom = exitDirection.Left;
+
 function Start() {
   fizDoor = GetComponent(Transform).parent;
 }
 
 function OnTriggerExit2D(coll : Collider2D) {
-  if (coll.gameObject.tag == "TheGuy" && freezeOnExit) {
+  if (coll.gameObject.tag == "TheGuy" && freezeOnExit && exitedInCorrectDirection(coll)) {
     StartCoroutine("disableAnimation");
     this.GetComponent(BoxCollider2D).isTrigger = false;
+    fizDoor.GetComponent(BoxCollider2D).isTrigger = true;
+    fizDoor.tag = "Ground";
     this.tag = "Ground";
   }
 }
-
 
 function disableAnimation() {
   var animatorObject = fizDoor.GetComponent(Animator);
@@ -28,6 +32,24 @@ function disableAnimation() {
     animatorObject.speed -= 0.01;
     yield WaitForSeconds(0.01);
   }
+}
 
-  // fizDoor.GetComponent(Animator).enabled = false;
+function exitedInCorrectDirection(coll : Collider2D) {
+  var correct : boolean = false;
+  switch (exitFrom)
+  {
+    case exitDirection.Down:
+      if (coll.bounds.center.y < this.transform.position.y) { correct = true; }
+      break;
+    case exitDirection.Up:
+      if (coll.bounds.center.y > this.transform.position.y) { correct = true; }
+      break;
+    case exitDirection.Right:
+      if (coll.bounds.center.x > this.transform.position.x) { correct = true; }
+      break;
+    case exitDirection.Left:
+      if (coll.bounds.center.x < this.transform.position.x) { correct = true; }
+      break;
+  }
+  return correct;
 }
