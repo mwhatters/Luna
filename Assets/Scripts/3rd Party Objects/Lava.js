5a5bc95ebdbdfff2ad284e2f653e48fe6efc0ba3
mwhatters <mwhatters@gmail.com>
2017-01-08 22:02:53 -0800

    lava level almost done

--- Assets/Scripts/3rd Party Objects/Lava.js
+++ Assets/Scripts/3rd Party Objects/Lava.js
@@ -5,9 +5,29 @@ public var active : boolean = false;
 public var rate : float = 0.06;
 public var tracker : GameObject;
 
+public var backAndForthTimer : boolean = false;
+public var time : float = 5;
+
+private var nextReverse : float = 0;
+private var reversed : boolean = false;
+
+function Start() {
+  nextReverse = 0;
+  reversed = false;
+}
+
 function FixedUpdate() {
 
   if (!active) { return false; }
+
+  if (backAndForthTimer) {
+    if (Time.time > nextReverse) {
+      reverseDirections();
+      reversed = !reversed;
+      nextReverse += time;
+    }
+  }
+
   if (lavaDir == "up") {
     adjustLava(Vector3.up);
   } else if (lavaDir == "down") {
@@ -19,6 +39,25 @@ function FixedUpdate() {
   }
 
   return true;
+}
+
+
+function reverseDirections() {
+  switch (lavaDir)
+  {
+    case "up":
+      lavaDir = "down";
+      break;
+    case "down":
+      lavaDir = "up";
+      break;
+    case "left":
+      lavaDir = "right";
+      break;
+    case "right":
+      lavaDir = "left";
+      break;
+  }
 }
 
 function adjustLava(vector : Vector3) {
