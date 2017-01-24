444341ad26492c4ab1a272c2060bd5f4ee2b2d6d
mwhatters <mwhatters@gmail.com>
2017-01-23 23:43:08 -0800

    lasers attempts

--- Assets/Scripts/3rd Party Objects/Clinger.js
+++ Assets/Scripts/3rd Party Objects/Clinger.js
@@ -7,18 +7,22 @@ public var previousRotation;
 
 public var active = false;
 public var rate : float = 100;
+public var rotationOnly : boolean = false;
 
 private var clingerbody : Rigidbody2D;
 private var musicPlaying : boolean = false;
 
+private var luna : Transform;
+
 function Start() {
   if (!Core) { Core = GameObject.Find("Luna"); }
   clingerbody = this.GetComponent(Rigidbody2D);
+  luna = GameObject.Find("Luna").transform;
 }
 
 function FixedUpdate () {
   if (active) {
-    Move();
+    Move(rotationOnly);
     if (!musicPlaying) {
       GetComponent(AudioSource).Play();
       musicPlaying = true;
@@ -31,7 +35,12 @@ function FixedUpdate () {
   }
 }
 
-function Move () {
-    clingerbody.velocity.x += (Core.transform.position.x - this.transform.position.x) / rate;
-    clingerbody.velocity.y += (Core.transform.position.y - this.transform.position.y) / rate;
+function Move (rotationOnly) {
+    if (rotationOnly) {
+      var m = (luna.position.y - this.transform.position.y)/(luna.position.x - this.transform.position.x);
+      this.transform.rotation = Vector2((luna.position.x - this.transform.position.x), (luna.position.y - this.transform.position.y));
+    } else {
+      clingerbody.velocity.x += (Core.transform.position.x - this.transform.position.x) / rate;
+      clingerbody.velocity.y += (Core.transform.position.y - this.transform.position.y) / rate;
+    }
 }
