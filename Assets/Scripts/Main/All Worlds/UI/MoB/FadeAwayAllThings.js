253315fc5e1dcdc1c1dfbbc5ed331ccf661678d1
mwhatters <mwhatters@gmail.com>
2017-02-26 12:59:01 -0800

    changes

--- Assets/Scripts/Main/All Worlds/UI/MoB/FadeAwayAllThings.js
+++ Assets/Scripts/Main/All Worlds/UI/MoB/FadeAwayAllThings.js
@@ -25,10 +25,19 @@ function OnTriggerEnter2D(coll : Collider2D) {
 
     gravity.cameraRotationEnabled = false;
     Sounds.use.FadeOut("DrowningAtSea", 0.0015, 0.0);
+    Sounds.use.FadeOut("Wind", 0.02, 0.0);
     camObj.GetComponent(CameraBehavior).FadeToColor(Color.black, 0.011);
+
+    var clingers = GameObject.Find("ClingersR3");
+    var clingerKids : Component[] = clingers.GetComponentsInChildren(Transform);
+    for (var child : Transform in clingerKids) {
+      if (child == clingers.transform) { continue; }
+      child.GetComponent(Clinger).active = false;
+    }
+
 
     yield WaitForSeconds(7);
 
-    SceneHelper.use.ShowAndHideText(Text1, 10);
+    SceneHelper.use.ShowAndHideText(Text1, 8);
   }
 }
