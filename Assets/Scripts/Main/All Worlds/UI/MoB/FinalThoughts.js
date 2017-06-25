23177c5d11ae32937b7530d2773dfe106a469893
mwhatters <mwhatters@gmail.com>
2017-06-25 00:59:43 -0700

    mob lobby lines

---
+++ Assets/Scripts/Main/All Worlds/UI/MoB/FinalThoughts.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+var Text1 : GameObject;
+var triggered : boolean = false;
+
+function Start () {
+	Text1 = GameObject.Find("Text1");
+}
+
+function OnTriggerEnter2D(coll : Collider2D) {
+
+    if (coll.name == "Luna" && !triggered) {
+			triggered = true;
+			SceneHelper.use.ShowAndHideText(Text1, 7);
+		}
+
+	}
