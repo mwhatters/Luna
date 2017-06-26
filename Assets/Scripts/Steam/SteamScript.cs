8e762d42fcecbfe3cc467af07f73b240dd9a73e9
mwhatters <mwhatters@gmail.com>
2017-06-26 12:33:25 -0700

    initial steam work

---
+++ Assets/Scripts/Steam/SteamScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Steamworks;
+
+public class SteamScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// if(SteamManager.Initialized) {
+		// 	string name = SteamFriends.GetPersonaName();
+		// 	Debug.Log(name);
+		// }
+	}
+
+}
